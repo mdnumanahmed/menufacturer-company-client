a8466f63c830927c20d82342d185fa554a99cf43
numansaady <numansaady@gmail.com>
2022-05-24 06:50:59 +0600

    nested router added in Dashboard

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
+
+const Dashboard = () => {
+    const [user] =  useAuthState(auth);
+    // const [admin] = useAdmin(user);
+    return (
+        <div className="drawer drawer-mobile">
+            <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                <h2 className='text-2xl font-bold text-purple-500'>Welcome to {user.email || user.displayName}'s Dashboard</h2>
+                <Outlet></Outlet>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
+                    <li><Link to="/dashboard">My Profile</Link></li>
+                    <li><Link to="/dashboard/my-orders">My Orders</Link></li>
+                        <li><Link to="/dashboard/add-review">Add Review</Link></li>
+                        <li><Link to="/dashboard/make-admin">Make Admin</Link></li>
+                        <li><Link to="/dashboard/add-product">Add Product</Link></li>
+                        <li><Link to="/dashboard/manage-product">Manage Products</Link></li>
+                        <li><Link to="/dashboard/manage-all-orders">Manage All Orders</Link></li>
+                    
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
